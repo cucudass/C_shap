0402d4f70b3ec59019e9ac5f42881cd44c45c628
조광래 <whrhkdfo12@naver.com>
2023-11-13 19:47:45 +0900

    231113_인터페이스, 추상클래스

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -5,8 +5,72 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Program {
+    public interface IItem {
+        void Use();
+    }
+    public interface IWaitTime {
+        void Wait(float timer);
+    }
+
+    public class Player {
+        public void OnTriggerEnter(IItem item) {
+            item.Use();
+        }
+    }
+    public class GameObject {
+        private int guID;
+
+        public int GUID {
+            set { guID = value; }
+            get { return guID; }
+        }
+
+        public GameObject Clon() {
+            // 1.class를 생성한다.
+            GameObject clon = new GameObject();
+            // 2.class 안에 있는 데이터를 넣어준다.
+            clon.guID = GUID;
+            // 3.class 를 반환한다.
+            return clon;
+        }
+
+    }
+
     class Program {
         static void Main(string[] args) {
+            #region 얕은 복사
+            //GameObject gameObject1 = new GameObject();
+            //GameObject gameObject2 = gameObject1.Clon();
+            //gameObject2.GUID = 99;
+            //Console.WriteLine("gameObject1의 GUID: " + gameObject1.GUID);
+            //Console.WriteLine("gameObject2의 GUID: " + gameObject2.GUID);
+            #endregion
+
+            #region 깊은 복사
+            //GameObject clon1 = new GameObject();
+            //clon1.GUID = 1;
+            //GameObject clon2 = clon1.Clon();
+            //clon2.GUID = 3;
+            //Console.WriteLine("clon1의 GUID: " + clon1.GUID);
+            //Console.WriteLine("clon2의 GUID: " + clon2.GUID);
+            #endregion
+
+            #region 인터페이스
+            /*
+             인터페이스 참조 변수를 선언할 수 있지만, 인스턴스를 생성할 수 없다.
+             */
+            //Player player = new Player();
+            //player.OnTriggerEnter(new Magnet());
+            //player.OnTriggerEnter(new Shield());
+            //player.OnTriggerEnter(new Missile());
+            #endregion
+
+            #region 추상클래스
+            Juggling juggling = new Juggling();
+            juggling.Information();
+            juggling.Skill();
+            #endregion
+
         }
     }
 }
---
+++ Program/Program/Shield.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program {
+    public class Shield : IItem, IWaitTime {
+        public void Use() {
+            Console.WriteLine("Use Shield");
+        }
+
+        public void Wait(float timer) {
+            Console.WriteLine("Wait Time: " + timer);
+        }
+    }
+}
