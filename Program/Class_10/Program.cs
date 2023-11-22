3fa9f4ed06e9654699fa15eec993eadb222f568e
조광래 <whrhkdfo12@naver.com>
2023-11-22 19:48:08 +0900

    231122_무명형식

---
+++ Program/Class_10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_10 {
+    class Program {
+        static void Main(string[] args) {
+            #region 등차수열
+            //int a = 0;
+            //int d = 0;
+            //int cnt = 0;
+
+            //Console.Write("첫째 항: ");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("공차: ");
+            //d = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("반복 횟수: ");
+            //cnt = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= cnt; i++) {
+            //    Console.Write(a + (i - 1) * d + " ");
+            //}
+            #endregion
+
+            #region 등비수열
+            //Console.Write("첫 항: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("공비: ");
+            //int d = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("횟수: ");
+            //int cnt = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 0; i < cnt; i++) {
+            //    Console.WriteLine(n * (Math.Pow(d, i)));
+            //}
+            #endregion
+
+            #region 무명 형식
+            /*
+             * 이름이 존재하지 않는 객체 또는 변수를 의미
+             * 무명 형식의 경우 값을 읽기만 할 수 있다.
+             */
+            //var data = new { Name = "Data", Value = 10 };
+            //Console.WriteLine("data.Name: " + data.Name + ", data.Value: " + data.Value);
+            #endregion
+        }
+    }
+}
--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -4,10 +4,36 @@ using System.Collections.Generic;
 
 namespace Program {
     class Program {
+        public static void Search(int[] arr, int start, int end, int find) {
+            if (start <= end) {
+                int mid = (start + end) / 2;
+                if (arr[mid] == find) {
+                    Console.WriteLine("값이 존재.");
+                } else {
+                    if (arr[mid] > find)
+                        Search(arr, start, mid, find);
+                    else
+                        Search(arr, mid, end, find);
+                }
+            } else {
+                Console.WriteLine("값이 없음...");
+            }
+        }
         static void Main(string[] args) {
-            //등차수열
-            int n = Console.Read();
-            Console.WriteLine(n);
+            #region 이진 탐색
+            /*
+             * 오름차순으로 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘
+             */
+            int[] arr = new int[5] { 3, 2, 5, 4, 1 };
+            int n = 0;
+            Array.Sort(arr);
+
+            Console.Write("찾으려는 값: ");
+            n = Convert.ToInt32(Console.Read());
+
+            Search(arr, 0, arr.Length, n);
+
+            #endregion
         }
     }
 }
