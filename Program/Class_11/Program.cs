943899666996de699b5cb06f373749f5eb2decea
조광래 <whrhkdfo12@naver.com>
2023-11-23 19:47:33 +0900

    231123_퀵정렬

---
+++ Program/Class_11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_11 {
+    class Program {
+        public static void Search(int[] arr, int start, int end, int find) {
+            if (start <= end) {
+                int mid = (start + end) / 2;
+                if (arr[mid] == find) {
+                    Console.WriteLine($"값({arr[mid]})이 존재.");
+                } else {
+                    if (arr[mid] > find)
+                        Search(arr, start, mid - 1, find);
+                    else
+                        Search(arr, mid + 1, end, find);
+                }
+            } else {
+                Console.WriteLine("값이 없음...");
+            }
+        }
+        static void Main(string[] args) {
+            #region 이진 탐색
+            /*
+             * 오름차순으로 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘
+             */
+            int[] arr = new int[5] { 3, 2, 5, 4, 1 };
+            int n = 0;
+            Array.Sort(arr); //숫자를 오름차순으로 정렬한다.
+
+            Console.Write("찾으려는 값: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Search(arr, 0, arr.Length - 1, n);
+
+            #endregion
+        }
+    }
+}
--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -4,35 +4,44 @@ using System.Collections.Generic;
 
 namespace Program {
     class Program {
-        public static void Search(int[] arr, int start, int end, int find) {
-            if (start <= end) {
-                int mid = (start + end) / 2;
-                if (arr[mid] == find) {
-                    Console.WriteLine("값이 존재.");
-                } else {
-                    if (arr[mid] > find)
-                        Search(arr, start, mid, find);
-                    else
-                        Search(arr, mid, end, find);
+        public static void Swap(ref int a, ref int b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        public static void QuickSort(int[] arr, int start, int end) {
+            int pivot = start;
+            int left = start + 1;
+            int right = end;
+
+            while (left <= right) {
+                while (arr[pivot] > arr[left]) {
+                    left++;
                 }
-            } else {
-                Console.WriteLine("값이 없음...");
+                while (arr[pivot] < arr[right]) {
+                    right--;
+                }
+                if(arr[left] > arr[right])
+                    Swap(ref arr[left], ref arr[right]);
             }
+            Swap(ref arr[pivot], ref arr[right]);
+            QuickSort(arr, start, right-1);
+            QuickSort(arr, right+1, end);
         }
         static void Main(string[] args) {
-            #region 이진 탐색
-            /*
-             * 오름차순으로 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘
+            #region 퀵정렬
+            /* 기준점을 획득한 다음 해당 기준점을 기준으로 배여을 나누고 한 쪽에는 기준점보다 작은 항목들이 위치하고
+             * 다른 쪽에는 기준점보다 끈 항목들이 위치한다.
+             * 
+             * 나뉘어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬하는 알고리즘
+             * 시간복잡도: O(log n)
              */
-            int[] arr = new int[5] { 3, 2, 5, 4, 1 };
-            int n = 0;
-            Array.Sort(arr);
+            int[] arr = new int[9] { 5, 3, 8, 4, 9, 1, 6, 2, 7 };
+            QuickSort(arr, 0, arr.Length - 1);
 
-            Console.Write("찾으려는 값: ");
-            n = Convert.ToInt32(Console.Read());
-
-            Search(arr, 0, arr.Length, n);
-
+            for (int i = 0; i < arr.Length; i++) {
+                Console.WriteLine(arr[i]);
+            }
             #endregion
         }
     }
